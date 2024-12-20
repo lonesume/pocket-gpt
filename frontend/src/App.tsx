88d24931ff66a289fa2330b541f19104a5b265fa
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-12-20 15:00:21 -0500

    Disable multiple calls on enter and search button press

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,69 @@ function App() {
   const [resultText, setResultText] = useState("");
   const [query, setQuery] = useState("");
   const [userName, setUserName] = useState("dear");
+  const [isGptResponseLoading, setIsGptResponseLoading] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
 
   const updateQuery = (e: any) => setQuery(e.target.value);
+
+  const getGPTResponse = useCallback(async () => {
+    if (!query) {
+      setResultText(
+        "Query cannot be empty, please enter something in the search bar"
+      );
+      return;
+    }
+
+    if (isGptResponseLoading || isTyping) {
+      return;
+    }
+
+    setIsGptResponseLoading(true);
+    const gptResponse = await GetGPTResponse(query);
+    setIsGptResponseLoading(false);
+
+    typeResponseGradually(gptResponse);
+  }, [query, isTyping, isGptResponseLoading]);
+
+  const typeResponseGradually = (message: string) => {
+    setResultText(""); // Clear any previous text
+    setIsTyping(true);
+    let index = 0;
+    const speed = 50;
+
+    const typeChar = () => {
+      if (index < message.length) {
+        setResultText((prev) => prev + message.charAt(index));
+        index++;
+        setTimeout(typeChar, speed);
+      } else {
+        setIsTyping(false);
+      }
+    };
+
+    typeChar();
+  };
+
+  // Handle keyboard events
+  const handleInputEvent = useCallback(
+    async (event: KeyboardEvent) => {
+      const key = event.key;
+      console.log(event);
+
+      switch (key) {
+        case "Enter":
+          if (isGptResponseLoading || isTyping) {
+            return;
+          }
+          await getGPTResponse();
+          return;
+        case "Escape":
+          console.log("closing");
+          return;
+      }
+    },
+    [query, isGptResponseLoading, isTyping, getGPTResponse]
+  );
 
   // Fetch username on component mount
   useEffect(() => {
@@ -22,56 +83,12 @@ function App() {
     fetchUserName();
   }, []);
 
-  // Handle keyboard events
-  const handleInputEvent = useCallback(
-    (event: KeyboardEvent) => {
-      const key = event.key;
-      console.log(event);
-
-      switch (key) {
-        case "Enter":
-          getGPTResponse();
-          return;
-        case "Escape":
-          console.log("closing");
-          return;
-      }
-    },
-    [query]
-  );
-
   useEffect(() => {
     document.addEventListener("keypress", handleInputEvent);
     return () => {
       document.removeEventListener("keypress", handleInputEvent);
     };
   }, [handleInputEvent]);
-
-  const getGPTResponse = useCallback(() => {
-    if (!query) {
-      alert("Query cannot be empty");
-      return;
-    }
-    GetGPTResponse(query).then(typeResponseGradually);
-  }, [query]);
-
-  function typeResponseGradually(message: string) {
-    setResultText(""); // Clear any previous text
-    let index = -1;
-    const speed = 50; // Speed of typing effect in milliseconds
-
-    function typeChar() {
-      if (index < message.length) {
-        setResultText((prev) => {
-          return prev + message.charAt(index);
-        });
-        index++;
-        setTimeout(typeChar, speed); // Recursively call to add the next character
-      }
-    }
-
-    typeChar(); // Start the typing effect
-  }
 
   return (
     <div id="App">
@@ -85,7 +102,11 @@ function App() {
           type="text"
           placeholder={`How can I help today, ${userName}?`}
         />
-        <button className="btn" onClick={getGPTResponse}>
+        <button
+          className="btn"
+          onClick={getGPTResponse}
+          disabled={isGptResponseLoading || isTyping}
+        >
           Search
         </button>
         <div id="result" className="result">
